8fdf7ceffa75898b7423e51c2c744ad8aff677a9
Piyush105454 <Piyushmodi812@gmail.com>
2026-08-06 18:44:38 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:8080', 'https://www.innoalaxy.in'], 
+  origin: ['http://localhost:8080', 'https://www.innoalaxy.in', 'https://resurrect-code.vercel.app'], 
   credentials: true 
 }));
 app.use(express.json());
